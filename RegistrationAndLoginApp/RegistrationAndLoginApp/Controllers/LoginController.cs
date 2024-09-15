30945f3b925e9a10708b04972bd9f25d1963b565
Filip Ebert <65273341+TemnyZlo@users.noreply.github.com>
2024-09-15 14:40:46 +0200

    log

---
+++ RegistrationAndLoginApp/RegistrationAndLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrationAndLoginApp.Models;
+
+namespace RegistrationAndLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+
+            if (userModel.UserName == "BillGates" && userModel.Password == "bigbucks")
+            {
+
+                return View("LoginSuccess", userModel);
+
+            } else
+            {
+                return View("LoginFailure", userModel);
+            }
+
+
+            
+        }
+
+
+    }
+}
